5c7c39f8de56eaeb0e2fbf336074cdf8013d053f
Tom Van Looy <tom@ctors.net>
2010-11-19 21:33:38 +0100

    my first AVR code, jey!

---
+++ 1/simpleAnimation.c
@@ -0,0 +1,28 @@
+/**
+ * MikroElektronika UNI-DS 3 development system
+ * MCU: AVR ATmega128
+ * Osc: 10 MHz
+ *
+ * Author: Tom Van Looy <tom$ctors,net>
+ * License: http://ctors.net/isc_license.txt
+ *
+ * This program plays a simple animation on the LED's on the board
+ */
+ 
+void main()
+{
+    unsigned int i;
+    DDRA = DDRB = DDRC = DDRD = DDRE = DDRF = 0xFF;
+
+    while (1) {
+        PORTA = PORTB = PORTC = PORTD = PORTE = PORTF = 0x00;
+
+        for (i = 1; i <= (0xFF + 1) * 8; i *= 2) {
+            Delay_ms(200);
+            PORTA = PORTF = i / 4 + (i / 8);
+            PORTB = PORTE = i / 2 + (i / 4);
+            PORTC = PORTD = i + (i / 2);
+        }
+    }
+}
+
