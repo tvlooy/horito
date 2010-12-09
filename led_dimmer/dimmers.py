923613984cf28fc608ce3b85afdebb8ae8b0ca69
Tom Van Looy <tom@ctors.net>
2010-12-09 23:39:31 +0100

    clean up

--- led_dimmer/dimmers.py
+++ led_dimmer/dimmers.py
@@ -15,20 +15,11 @@ class App:
        frame = Frame(master)
        frame.pack()
 
-       self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
-       self.button.grid(row=0, column=0)
-
        self.canvas1 = Canvas(frame)
        self.canvas1.grid(row=0, column=1)
 
-       self.hi_there = Button(frame, text="Hello", command=self.say_hi)
-       self.hi_there.grid(row=1, column=0)
-
        self.scale1 = Scale(frame, command=self.say_value)
-       self.scale1.grid(row=1, column=1)
-
-   def say_hi(self):
-       print "hi there, everyone!"
+       self.scale1.grid(row=0, column=0)
 
    def say_value(self, value):
        value = int(value) * 2.55;
