b452ce96d6757c8968c5b58b5299b1b469d79102
Tom Van Looy <tom@ctors.net>
2010-12-27 19:28:47 +0100

    add some more comments

--- 6_reactionTest/reactionTest.c
+++ 6_reactionTest/reactionTest.c
@@ -8,7 +8,7 @@
  *
  * On the board, set dip switches of PORTA, PORTB, PORTC and PORTE to OFF.
  *
- * This program is a reaction tester. When the LEDs of PORTB are turned on,
+ * This program is a reaction tester game. When the LEDs of PORTD are turned on,
  * the user must press button RD0 as quick as possible. The reaction speed is
  * displayed on the LCD.
  */
@@ -49,7 +49,7 @@ void setup(void)
     EICRA |= 0b00000011; // Trigger INT0 on rising edge
 
     TCCR0 = 0b00111100;  // Timer/counter register
-    OCR0 = 0x9B;         // Output compare
+    OCR0 = 0x9C;         // Output compare
     TIMSK |= 0b00000010; // Interrupt on compare match
     
     DDRD = 0xFF;         // Set the LED pins to output
@@ -89,8 +89,8 @@ void Timer0CompareMatch() iv IVT_ADDR_TIMER0_COMP
 void main(void)
 {
     char text[256] = "";
-
     setup();
+    
     Glcd_Write_Text("                     ", 0, 2, 0);
     Glcd_Write_Text("*** ARE YOU READY ***", 0, 3, 0);
     Glcd_Write_Text("                     ", 0, 4, 0);
@@ -104,6 +104,8 @@ void main(void)
     _finished = FALSE;
     _started = TRUE;
     PORTD = 0b11110000;
+    
+    // If user didn't cheat, flag start
     if (_cheater == FALSE) {
         Glcd_Fill(0x00);
         Glcd_Write_Text("        o_________", 0, 1, 1);
@@ -116,22 +118,26 @@ void main(void)
     }
 
     while(1) {
+        // If user cheated, show a message and stop game.
+        // Start flickering the LED array
         if (_cheater == TRUE && _finished == FALSE) {
-             PORTD = 0b01010101; //
-             Glcd_Fill(0x00);
-             Glcd_Write_Text("                     ", 0, 0, 0);
-             Glcd_Write_Text("***** KITTENS *******", 0, 1, 0);
-             Glcd_Write_Text("Every time you press ", 0, 3, 1);
-             Glcd_Write_Text("early ... God kills a", 0, 4, 1);
-             Glcd_Write_Text("kitten. Please, think", 0, 5, 1);
-             Glcd_Write_Text("of the kittens!      ", 0, 6, 1);
+            PORTD = 0b01010101;
+            Glcd_Fill(0x00);
+            Glcd_Write_Text("                     ", 0, 0, 0);
+            Glcd_Write_Text("***** KITTENS *******", 0, 1, 0);
+            Glcd_Write_Text("Every time you press ", 0, 3, 1);
+            Glcd_Write_Text("early ... God kills a", 0, 4, 1);
+            Glcd_Write_Text("kitten. Please, think", 0, 5, 1);
+            Glcd_Write_Text("of the kittens!      ", 0, 6, 1);
             _finished = TRUE;
         }
         if (_cheater == TRUE) {
             Delay_ms(500);
             PORTD = ~PORTD;
         }
-        
+
+        // If the user didn't press the button within 10 seconds, show a message
+        // and end the game. Start flickering the LED array
         if (_buttonPressed == TRUE && _finished == FALSE && _started == TRUE) {
             Glcd_Fill(0x00);
             if (_milliseconds > 10000) {
@@ -144,7 +150,10 @@ void main(void)
                 Glcd_Write_Text("        \\|   \\o__/(",  0, 5, 1);
                 Glcd_Write_Text("                \\  \\", 0, 6, 1);
                 _cheater = TRUE;
-            } else {
+            }
+            // If the user pressed the button, show his reaction time.
+            // End the game
+            else {
                 PORTD = 0b00001111;
                 sprintf(text, "Your score: %d ms    ", _milliseconds);
                 Glcd_Write_Text("***** FINISHED ****** ",  0, 0, 0);
