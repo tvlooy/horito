f9032f59a5060bf0e65151378e67d7c5cebd11ad
Tom Van Looy <tom@ctors.net>
2011-07-31 22:16:39 +0200

    port pwm_fade_led to gcc-avr

---
+++ gcc-avr/2_pwm_fade_led/pwmFadeLed.c
@@ -0,0 +1,46 @@
+/**
+ * MikroElektronika UNI-DS 3 development system
+ * MCU: AVR ATmega128
+ * Osc: 10 MHz
+ *
+ * Author: Tom Van Looy <tom$ctors,net>
+ * License: http://ctors.net/isc_license.txt
+ *
+ * This program fades a LED in and out using a timer to generate PWM
+ */
+
+#define F_CPU 10000000UL // 10 MHz
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void main(void)
+{
+    /**
+     * Timer/counter control register. The bits stand for:
+     * FOC0 WGM00 COM01 COM00 WGM01 CS02 CS01 CS00
+     */
+    TCCR0 = 0b01101001;
+    
+    /**
+     * Output compare register is used as counter
+     */
+    OCR0 = 0;
+    
+    /**
+     * OC0 is on pin PB4, which has to be set to output
+     */
+    DDRB = 0b00010000;
+    
+    while(1) {
+        while(OCR0 < 255) {
+            _delay_ms(5);
+            OCR0++;
+        }
+        while(OCR0 > 0) {
+            _delay_ms(10);
+            OCR0--;
+        }
+    }
+}
+
